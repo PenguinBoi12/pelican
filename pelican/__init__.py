6c64cb6695a286e40c584194b6b1011f386c7bef
penguinboi <simon.roy1211@gmail.com>
2025-12-13 16:17:23 -0500

    Separated operations and builder

--- pelican/__init__.py
+++ pelican/__init__.py
@@ -2,7 +2,7 @@
 
 from .migration import MigrationRegistry
 from .runner import MigrationRunner
-from .operations import create_table, change_table, drop_table
+from .schema import create_table, change_table, drop_table
 
 __version__ = "1.0.0-alpha"
 
---
+++ pelican/schema/__init__.py
@@ -0,0 +1,7 @@
+from .helpers import create_table, change_table, drop_table
+
+__all__ = [
+    "create_table",
+    "change_table",
+    "drop_table",
+]
