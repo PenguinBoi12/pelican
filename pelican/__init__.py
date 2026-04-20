b4d0937c186d37bf3b380ff879b7aa7eb2ed8991
penguinboi <simon.roy1211@gmail.com>
2026-04-20 02:10:38 -0400

    Added git-tag project versioning

--- pelican/__init__.py
+++ pelican/__init__.py
@@ -4,9 +4,15 @@ from .migration import MigrationRegistry
 from .runner import MigrationRunner
 from .schema import create_table, change_table, drop_table
 
-__version__ = "1.0.0-alpha"
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("pelican")
+except PackageNotFoundError:
+    from pelican._version import version as __version__
 
 __all__ = [
+    "__version__",
     "create_table",
     "change_table",
     "drop_table",
